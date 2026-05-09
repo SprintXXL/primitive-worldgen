bf00618d92a0a4740f2a2add93efe9f440671f2a
SprintXXL <mspmain@pm.me>
2026-05-09 05:39:13 +0200

    Disable vanilla underground water lake gen

--- src/main/java/com/SprintXXL/primitiveworldgen/handler/PopulateEventHandler.java
+++ src/main/java/com/SprintXXL/primitiveworldgen/handler/PopulateEventHandler.java
@@ -1,4 +1,20 @@
 package com.SprintXXL.primitiveworldgen.handler;
 
+import net.minecraftforge.event.terraingen.PopulateChunkEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import static com.SprintXXL.primitiveworldgen.config.WorldGenConfig.DISABLE_UNDERGROUND_WATER_LAKES;
+
+@Mod.EventBusSubscriber
 public class PopulateEventHandler {
+
+    @SubscribeEvent
+    public void PopulateEvent(PopulateChunkEvent.Populate event) {
+
+        if (event.getType() == PopulateChunkEvent.Populate.EventType.LAKE && DISABLE_UNDERGROUND_WATER_LAKES) {
+            event.setResult(Event.Result.DENY);
+        }
+    }
 }
